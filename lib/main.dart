de0b16025c5e77ba492272aad6f9fda85288ef1d
Design Leaders <designleaders.bd@gmail.com>
2021-12-09 13:45:26 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MusicPlay());
+}
+
+class MusicPlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [],
+        )),
+      ),
+    );
+  }
+}
